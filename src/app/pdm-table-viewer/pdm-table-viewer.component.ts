e39e1c05a4db4c9e2e6382ac86a6f25812fd8175
doricat <doricat@outlook.com>
2021-01-08 23:34:45 +0800

    简单实现表格复制

--- src/app/pdm-table-viewer/pdm-table-viewer.component.ts
+++ src/app/pdm-table-viewer/pdm-table-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Table as PdmTable } from '../models/pdmModels';
 import { TableService } from '../table.service';
@@ -14,6 +14,8 @@ export class PdmTableViewerComponent implements OnInit, OnDestroy {
     table: Table | undefined;
     serviceSubscription: Subscription | null;
     isMouseDown = false;
+    @ViewChild('clipboardData') clipboardData: ElementRef;
+    @ViewChild('tableRef') tableRef: ElementRef;
 
     constructor(private tableService: TableService) { }
 
@@ -23,17 +25,22 @@ export class PdmTableViewerComponent implements OnInit, OnDestroy {
             this.table = undefined;
 
             if (x) {
-                this.table = new Table(x.columns.length === 0 ? 1 : x.columns.length, 7);
+                const array: string[][] = [];
                 for (let i = 0; i < x.columns.length; i++) {
                     const column = x.columns[i];
-                    this.table.cells[i][0].content = column.name;
-                    this.table.cells[i][1].content = column.code;
-                    this.table.cells[i][2].content = column.comment;
-                    this.table.cells[i][3].content = column.dataType;
-                    this.table.cells[i][4].content = column.length;
-                    this.table.cells[i][5].content = column.precision;
-                    this.table.cells[i][6].content = String(column.mandatory);
+                    const row: string[] = [
+                        column.name,
+                        column.code,
+                        column.comment,
+                        column.dataType,
+                        column.length,
+                        column.precision,
+                        String(column.mandatory)
+                    ];
+                    array.push(row);
                 }
+
+                this.table = Table.from2DArray(array);
             }
         });
     }
@@ -59,6 +66,34 @@ export class PdmTableViewerComponent implements OnInit, OnDestroy {
         if (this.isMouseDown) {
             this.isMouseDown = false;
         }
+    }
+
+    onKeydown(evt: KeyboardEvent): void {
+        if (evt.ctrlKey && evt.key === 'c') {
+            const data = this.table.generateClipboardData();
+            const element = this.clipboardData.nativeElement as HTMLInputElement;
+            const table = Table.from2DArray(data);
+            const clipboardData = {};
+            clipboardData['text/plain'] = table.generatePlainTextTable();
+            clipboardData['text/html'] = this.generateHtml(data);
+            element.value = JSON.stringify(clipboardData);
+            element.select();
+            element.setSelectionRange(0, element.value.length);
+            document.execCommand('copy');
+        }
+    }
+
+    copy(evt: ClipboardEvent): void {
+        const element = evt.currentTarget as HTMLInputElement;
+        const json = element.value;
+        const obj = JSON.parse(json) as { [type: string]: string };
+        for (const key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                evt.clipboardData.setData(key, obj[key]);
+            }
+        }
+
+        evt.preventDefault();
     }
 
     selectCells(element: Element): void {
@@ -90,5 +125,36 @@ export class PdmTableViewerComponent implements OnInit, OnDestroy {
         }
 
         return null;
+    }
+
+    generateHtml(array: string[][]): string {
+        const result = `<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+</head>
+<body>
+    <table style="border-collapse: collapse; display: table; text-indent: initial; border-spacing: 2px; width: 100%; margin-bottom: 1rem; color: #212529; border: 1px solid #dee2e6;">
+        <tbody style="display: table-row-group; vertical-align: middle; border-color: inherit;">
+           ${this.generateCells(array)}
+        </tbody>
+    </table>
+</body>
+</html>`;
+        return result;
+    }
+
+    generateCells(array: string[][]): string {
+        const result: string[] = [];
+        for (let i = 0; i < array.length; i++) {
+            const lineItems: string[] = ['<tr style="display: table-row; vertical-align: inherit; border-color: inherit;">'];
+            for (let j = 0; j < array[i].length; j++) {
+                lineItems.push(`<td style="border: 1px solid #dee2e6; width: 50px; padding: 0.75rem; vertical-align: top;">${array[i][j] ?? ' '}</td>`);
+            }
+            lineItems.push('</tr>');
+            result.push(lineItems.join('\r\n'));
+        }
+
+        return result.join('\r\n');
     }
 }
