a1a853addbe7dd28d8a23b1e42fa4b93d6769b4e
doricat <8497937+doricat@users.noreply.github.com>
2021-04-05 22:41:25 +0800

    update dir struct

--- frontend/src/pages/PDM/PDMObjectViewer.tsx
+++ frontend/src/pages/PDM/PDMObjectViewer.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -36,24 +33,15 @@ export const PDMObjectViewer = () => {
             aria-labelledby="nested-list-subheader"
             className={classes.root}>
             <ListItem button onClick={handleClick}>
-                <ListItemIcon>
-                    <InboxIcon />
-                </ListItemIcon>
                 <ListItemText primary="Inbox" />
                 {open ? <ExpandLess /> : <ExpandMore />}
             </ListItem>
             <Collapse in={open} timeout="auto" unmountOnExit>
                 <List component="div" disablePadding>
                     <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <StarBorder />
-                        </ListItemIcon>
                         <ListItemText primary="Starred" />
                     </ListItem>
                     <ListItem button className={classes.nested}>
-                        <ListItemIcon>
-                            <StarBorder />
-                        </ListItemIcon>
                         <ListItemText primary="Starred" />
                     </ListItem>
                 </List>
--- frontend/src/pages/PDM/PDMTableViewer.tsx
+++ frontend/src/pages/PDM/PDMTableViewer.tsx
@@ -39,4 +39,4 @@ export const PDMTableViewer = () => {
             </Table>
         </TableContainer>
     );
-}
+};
