379366d83814fa5f160105ee21f028a531cc0d3a
doricat <8497937+doricat@users.noreply.github.com>
2021-04-12 23:48:36 +0800

    update ui

--- frontend/src/shared/Layout.tsx
+++ frontend/src/shared/Layout.tsx
@@ -1,144 +1,48 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, CssBaseline, Drawer, AppBar, Toolbar, Typography, Divider, IconButton, Container } from '@material-ui/core';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-
+import { makeStyles, CssBaseline, AppBar, Toolbar, Typography, IconButton, Container } from '@material-ui/core';
 import GitHub from '@material-ui/icons/GitHub';
 import Settings from '@material-ui/icons/Settings';
-
-const drawerWidth = 240;
+import { NavMenu } from './NavMenu';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        display: 'flex'
-    },
     toolbar: {
         paddingRight: 24 // keep right padding when drawer closed
-    },
-    toolbarIcon: {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        padding: '0 8px',
-        ...theme.mixins.toolbar
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen
-        }),
-    },
-    menuButton: {
-        marginRight: 36
-    },
-    menuButtonHidden: {
-        display: 'none'
     },
     title: {
         flexGrow: 1
     },
-    drawerPaper: {
-        position: 'relative',
-        whiteSpace: 'nowrap',
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen
-        }),
-    },
-    drawerPaperClose: {
-        overflowX: 'hidden',
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen
-        }),
-        width: theme.spacing(7),
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9)
-        },
-    },
-    appBarSpacer: theme.mixins.toolbar,
-    content: {
-        flexGrow: 1,
-        height: '100vh',
-        overflow: 'auto'
-    },
     container: {
         paddingTop: theme.spacing(4),
         paddingBottom: theme.spacing(4)
-    },
-    paper: {
-        padding: theme.spacing(2),
-        display: 'flex',
-        overflow: 'auto',
-        flexDirection: 'column'
     }
 }));
 
 export const Layout = (props: { children: React.ReactChild; }) => {
     const classes = useStyles();
-    const [open, setOpen] = React.useState(false);
-    const handleDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleDrawerClose = () => {
-        setOpen(false);
-    };
 
     return (
-        <div className={classes.root}>
+        <>
             <CssBaseline />
-            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+            <AppBar position="relative">
                 <Toolbar className={classes.toolbar}>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}>
-                        <MenuIcon />
-                    </IconButton>
                     <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
                         My-Toolkit
                     </Typography>
-                    <IconButton aria-label="" color="inherit">
+                    <NavMenu />
+                    <IconButton color="inherit">
                         <Settings />
                     </IconButton>
-                    <IconButton aria-label="" color="inherit" href="https://github.com/doricat/my-toolkit">
+                    <IconButton color="inherit" href="https://github.com/doricat/my-toolkit">
                         <GitHub />
                     </IconButton>
                 </Toolbar>
             </AppBar>
-            <Drawer
-                variant="permanent"
-                classes={{
-                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose)
-                }}
-                open={open}>
-                <div className={classes.toolbarIcon}>
-                    <IconButton onClick={handleDrawerClose}>
-                        <ChevronLeftIcon />
-                    </IconButton>
-                </div>
-                <Divider />
-                <Divider />
-            </Drawer>
-            <main className={classes.content}>
-                <div className={classes.appBarSpacer} />
-                <Container maxWidth="xl" className={classes.container}>
+
+            <main>
+                <Container maxWidth={false} className={classes.container}>
                     {props.children}
                 </Container>
             </main>
-        </div>
+        </>
     );
 };
---
+++ frontend/src/shared/NavMenu.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { makeStyles, Link } from '@material-ui/core';
+import {Link as RouterLink} from 'react-router-dom'
+
+const useStyles = makeStyles((theme) => ({
+    link: {
+        margin: theme.spacing(1, 1.5),
+        color: '#fff',
+        textTransform: 'none'
+    }
+}));
+
+export const NavMenu: React.FC = () => {
+    const classes = useStyles();
+
+    return (
+        <nav>
+            <Link variant="button" color="textPrimary" className={classes.link} component={RouterLink} to="/pdm-tools">
+                PDM-Tools
+            </Link>
+        </nav>
+    );
+};
